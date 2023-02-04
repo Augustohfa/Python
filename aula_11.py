774ab8bb0be4dd5b55eeefe4a93cd4498f5bea13
Augustohfa <azevedogestoes@gmail.com>
2023-02-04 13:42:45 -0300

    Aula 11 sobre ifs, elif e else

---
+++ aula_11.py
@@ -0,0 +1,29 @@
+#Continuação aula if // elif / else
+
+
+condicao = False
+
+if condicao:
+    print('Esse código é o código do if')
+
+else :
+    print('Esse código do else')
+
+print('Fora do if')
+
+
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print('Código para condição 1')
+else:
+    ('Nenhuma condição foi satisfeita')
+
+if condicao2:
+    print('Código para condição 2')
+else:
+    ('Nenhuma condição foi satisfeita')
+# Exemplo da aula, porém seria melhor usar elif 
