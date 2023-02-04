882566976e8fa078d77e0b638c3bf15b47624f42
Augustohfa <azevedogestoes@gmail.com>
2023-02-03 23:04:49 -0300

    Aulinha extra

---
+++ aula_list.py
@@ -0,0 +1,5 @@
+#Chat GPT me indicou que haviam um tipo de data que não tinha sido introduzida no curso, então decidi ir atras, list
+
+
+list_1 = list(input('Digite alguns valores!'))
+print(list_1[2])
