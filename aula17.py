3f2d5b373511c4e4b9cab55c69ebc2b62883a2d6
Augustohfa <azevedogestoes@gmail.com>
2023-02-05 18:18:19 -0300

    Eu amo minha noiva

---
+++ aula17.py
@@ -0,0 +1,15 @@
+"""
+Interpolação basica de strings
+s - string
+d e i = int
+f - float
+x e X = Hexadecimal
+"""
+nome = 'Luiz'
+preco = 1000.95897643
+variavel = '%s, o preço é R$%.2f' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %08X' % (20032, 20032))
+
+
+#continua muito mais facil utilizar f strings
