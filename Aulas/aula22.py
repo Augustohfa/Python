c8bd5e669d1d52dc3a5c5ce779c30aaeedcdd649
Augustohfa <azevedogestoes@gmail.com>
2023-02-09 21:25:14 -0300

    Id

--- Aulas/aula22.py
+++ Aulas/aula22.py
@@ -2,9 +2,11 @@
 Flag (bandeira) - marcar um local
 Nome = não há valor
 is e is not = é ou não é (tipo, valor, identidade)
+id = identidade 
 """
 
 v1 = 'a'
 v2 = 'a'
 print(id(v1))
 print(id(v2))
+
