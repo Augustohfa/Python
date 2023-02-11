04e507e18432c859e4403f025e5b3b9bffc69dc0
Augustohfa <azevedogestoes@gmail.com>
2023-02-11 16:46:21 -0300

    exercicio interando string com while

--- Exercícios/exercicio8.py
+++ Exercícios/exercicio8.py
@@ -6,11 +6,11 @@ Interando string com while
 
 nome = 'Augusto Azevedo'  # Interaveis
 tamanho_nome = len(nome)
-contador = 0
+indice_contador = 0
 nova_string = ''
 
-while contador < len(nome):
-    nova_string = nova_string + nome[contador] + '*'
-    contador += 1
+while indice_contador < len(nome):
+    nova_string = nova_string + nome[indice_contador] + '*'
+    indice_contador += 1
 
 print(nova_string)
