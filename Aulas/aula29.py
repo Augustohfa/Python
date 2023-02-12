25a342df072bbe2f8df327f9297b70d6c3fb204a
Augusto Henrique Ferreira de Azevedo <azevedogestoes@gmail.com>
2023-02-12 19:28:22 -0300

    Contador de letras de uma frase, usando while e interação de string

--- Aulas/aula29.py
+++ Aulas/aula29.py
@@ -1,5 +1,24 @@
-frase = 'O python é uma linguagem de programação' \
-    'multiparadigma.'  \
-    'Python foi criado por Guido van Rossum.'
+frase = input('Digite a frase que você quer saber qual'
+              'letra aparece mais letras: ')
 
-print(frase.lower().count('o'))
+# Indice
+i = 0
+# Variables
+qtd_apareceu_fora = 0
+letra_apareu_mais_vezes = ''
+# while function
+while i < len(frase):
+    letra_atual = frase[i]
+    if letra_atual == ' ':
+        i += 1
+        continue
+    qtd_apareceu_dentro = frase.count(letra_atual)
+
+    if qtd_apareceu_fora <= qtd_apareceu_dentro:
+        qtd_apareceu_fora = qtd_apareceu_dentro
+        letra_apareu_mais_vezes = letra_atual
+    i += 1
+
+# User return
+print(f'A letra que apareceu mais vezes foi:"{letra_apareu_mais_vezes}"'
+      f' aparecendo um total de {qtd_apareceu_fora} vezes')
