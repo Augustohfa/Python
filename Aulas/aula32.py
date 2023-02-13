196d377ac0ab594bd73cf8657e2341842c09a9fa
Augusto Henrique Ferreira de Azevedo <azevedogestoes@gmail.com>
2023-02-12 23:29:55 -0300

    Como funciona o for literalmente

---
+++ Aulas/aula32.py
@@ -0,0 +1,30 @@
+"""
+Iterável - > str, range, etc
+Iterador - > quem sabe entregar um valor por vez
+next - > me entrega o próximo valor
+iter - > me entregue seu iterador
+"""
+# texto = iter('Augusto')  # __iter__() "entregador"
+# # texto.metodo
+# print(texto.__next__())
+# print(texto.__next__())
+# print(texto.__next__())
+# print(texto.__next__())
+# print(texto.__next__())
+# print(texto.__next__())
+# print(texto.__next__())  # chama o próximo valor
+
+
+# for letra in texto   como funciona por baixo dos panos
+########################################################################
+texto = 'Augusto'  # iterável
+iterador = iter(texto)  # iterador
+
+while True:
+    try:
+        letra = next(iterador)
+        print(letra)
+    except StopIteration:
+        break
+# For faz exatamente isso "por baixo dos panos"
+########################################################################
