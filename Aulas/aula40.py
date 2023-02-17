b5990efc09ae7e078c51fcca44b6267f3c654200
Augusto Henrique Ferreira de Azevedo <azevedogestoes@gmail.com>
2023-02-16 21:59:26 -0300

    Indices in for exerciciozinho

---
+++ Aulas/aula40.py
@@ -0,0 +1,8 @@
+lista = ['Maria', 'Helena', 'Augusto']
+
+# Verifica quantos indices tem na lista
+
+indices = range(len(lista))
+# Mostra item por item no for
+for i in indices:
+    print(i, lista[i])
