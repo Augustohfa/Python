b2c83762774e6f0f6d52b04cec982905f43070f1
Augusto Henrique Ferreira de Azevedo <azevedogestoes@gmail.com>
2023-03-05 15:18:17 -0300

    aula return Funções python

---
+++ AulasModulo2/AulaReturn.py
@@ -0,0 +1,15 @@
+"""
+Retorno de uma função(return)
+"""
+variavel = print('Luiz')
+print(variavel)
+# Print é uma função embutida no python que não retorna nada
+# Logo o valor dela é None
+
+
+def soma(x, y):
+    return x + y
+
+
+soma1 = soma(1, 1)
+print(soma1)
