ebef479615555c799215474a0eba1637d076da36
Augusto Henrique Ferreira de Azevedo <azevedogestoes@gmail.com>
2023-03-14 01:28:27 -0300

    Testando uma extenção do VSCode

---
+++ TesteBetterComments.py
@@ -0,0 +1,8 @@
+print('Qualquer coisa do código')
+# ! Comentário em vemelho
+# TODO Anotar coisas pra fazer no código
+# ? Comentário em azul
+# * Comentário verde
+# // Riscar linha (Exemplo de uso - TODO já foi feito)
+
+# ! Testando se pode fazre dois na mesma linha *
