a45e5dd9279dd6ba2786971483e304617b961346
Augusto Henrique Ferreira de Azevedo <azevedogestoes@gmail.com>
2023-05-25 14:56:45 -0300

    Aula Init e como usar um pacote como módulo

---
+++ AulasModulo2/AulaInit.py
@@ -0,0 +1,7 @@
+import pacote_aula
+#  assim que importado o pacote
+# o arquivo __init__.py é executado
+# Agora o package é considerado um módulo pelo Python
+# como no __init__.py foi importado o modulo modulo.py
+# agora tudo dentro dele poderá ser usado nesse arquivo
+# apenas importando o package acote_aula
---
+++ AulasModulo2/pacote_aula/__init__.py
@@ -0,0 +1,2 @@
+from .modulo import divide_module
+print(f'Package {__name__} has been imported')
