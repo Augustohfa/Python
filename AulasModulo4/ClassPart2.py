bda76170fa00acb74c1328370cfa3c57ab5cc647
Augustohfa <azevedogestoes@gmail.com>
2023-08-23 02:57:36 -0300

    Aula sobre estados dentro de classes

--- AulasModulo4/ClassPart2.py
+++ AulasModulo4/ClassPart2.py
@@ -16,3 +16,40 @@ class Animal:
 leao = Animal(nome='Leão')
 print(leao.nome)
 print(leao.comendo('Carne'))
+
+#Mantendo estados dentro da classe
+
+
+class Cam:
+    def __init__(self, name, recording=False):
+        self.name = name
+        self.recording = recording
+
+    def record(self):
+        if self.recording:
+            print(f'{self.name} is already recording...')
+            return
+        print(f'{self.name} has began to record...')
+        self.recording = True
+
+        if not self.recording:
+            print(f'{self.name} is not recording...')
+            return
+        print(f'{self.name} has stopped the recording...')
+        self.recording = False
+
+
+cam_1 = Cam('Iphone 13 Pro Max')
+cam_2 = Cam('Logitech c920')
+
+cam_1.record()
+cam_1.record()
+
+cam_2.record()
+cam_2.record()
+
+cam_1.stopRecording()
+cam_1.stopRecording()
+
+cam_2.stopRecording()
+cam_2.stopRecording()
